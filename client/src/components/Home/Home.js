95f2b48c116284c459de8dac97f8bfe27a94d776
dinnf <finn.denis@gmail.com>
2023-04-02 14:47:59 +1200

    changed view notes to remove all textareas

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -88,6 +88,7 @@ const Home = () => {
                                 
                                 </Link>
                                 </td>
+                                <hr />
                                 </tr>
                     
                         ))}</tbody></table>{" "}
